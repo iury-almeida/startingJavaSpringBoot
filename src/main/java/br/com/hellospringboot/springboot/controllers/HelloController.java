d879ef864a87bad6b4eb781854135e9964eb7549
Iury Almeida <iurygh@gmail.com>
2019-01-08 10:57:29 -0200

    Configurando e utilizando a viem-engine Thymeleaf

---
+++ src/main/java/br/com/hellospringboot/springboot/controllers/HelloController.java
@@ -0,0 +1,14 @@
+package br.com.hellospringboot.springboot.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+	@RequestMapping("/")
+	public String hello(Model model) {
+		model.addAttribute("mensagem", "Olá Spring Boot com Thymeleaf");
+		return "index";
+	}
+}
